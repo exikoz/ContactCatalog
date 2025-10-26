c1affb4491c459c02f38b95491d4239c0b2688bd
exikoz <hussein.hasnawy@gmail.com>
2025-10-26 22:06:01 +0100

    test: added the rest of the missing tests

--- ContactCatalog.Tests/EmailValidatorTests.cs
+++ ContactCatalog.Tests/EmailValidatorTests.cs
@@ -1,10 +1,21 @@
+using ContactCatalog.Validators;
+
 namespace ContactCatalog.Tests;
 
 public class EmailValidatorTests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("test@example.com", true)]
+    [InlineData("user.name@domain.se", true)]
+    [InlineData("invalid-email", false)]
+    [InlineData("@example.com", false)]
+    [InlineData("", false)]
+    public void IsValidEmail_ValidatesCorrectly(string email, bool expected)
     {
+        // Act
+        var result = EmailValidator.IsValidEmail(email);
 
+        // Assert
+        Assert.Equal(expected, result);
     }
 }
