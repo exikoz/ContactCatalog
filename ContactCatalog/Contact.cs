9465184c31a14870528e5c61097a11a58e7b06be
exikoz <hussein.hasnawy@gmail.com>
2025-10-26 15:25:23 +0100

    feat: added Contact model with basic properties

---
+++ ContactCatalog/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactCatalog
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public List<string> Tags { get; set; } = new();
+
+        public override string ToString()
+        {
+            var tagsString = Tags.Count > 0 ? string.Join(", ", Tags) : "no tags";
+            return $"({Id}) {Name} <{Email}> [{tagsString}]";
+        }
+    }
+}
---
+++ ContactCatalog/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ContactCatalog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Contact Catalog - Testing Contact Model ===\n");
+
+            var contact1 = new Contact
+            {
+                Id = 1,
+                Name = "John Doeson",
+                Email = "j.d@test.com",
+                Tags = new List<string> { "firend", "gym" }
+
+            };
+
+            var contact2 = new Contact
+            {
+                Id = 2,
+                Name = "Bo Bengtsson",
+                Email = "bo@example.com",
+                Tags = new List<string> { "work", "colleague" }
+            };
+
+            var contact3 = new Contact
+            {
+                Id = 3,
+                Name = "Cecilia Carlsson",
+                Email = "cecilia@example.com",
+                Tags = new List<string>()
+            };
+
+            Console.WriteLine("Test Contacts");
+            Console.WriteLine(contact1);
+            Console.WriteLine(contact2);
+            Console.WriteLine(contact3);
+
+            Console.WriteLine("\n Contact working");
+
+        }
+    }
+}
